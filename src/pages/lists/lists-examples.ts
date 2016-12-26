23e9b62c1bf4f12393a0bba2cac42ef5d2f53f88
pearpages <pearpages@DarkMatter.local>
2016-12-26 10:32:44 +0100

    lists page

--- src/pages/lists/lists-examples.ts
+++ src/pages/lists/lists-examples.ts
@@ -6,7 +6,34 @@ import { Component } from '@angular/core';
   templateUrl: 'lists-examples.html'
 })
 export class ListsExamples {
+
+  items = [
+    'Pokémon Yellow',
+    'Super Metroid',
+    'Mega Man X',
+    'The Legend of Zelda',
+    'Pac-Man',
+    'Super Mario World',
+    'Street Fighter II',
+    'Half Life',
+    'Final Fantasy VII',
+    'Star Fox',
+    'Tetris',
+    'Donkey Kong III',
+    'GoldenEye 007',
+    'Doom',
+    'Fallout',
+    'GTA',
+    'Halo'
+  ];
+
+  selected: string;
+
   constructor() {
 
+  }
+
+  itemSelected(item) {
+    this.selected = item;
   }
 }
